05107ce93f0bdf27f76bae0ad69204e46f74ec7e
Gimun Lee <gimunlee@kaist.ac.kr>
2016-09-17 21:10:40 +0900

    primitive server and client with fixed message Hello, once.

--- client.c
+++ client.c
@@ -1,6 +1,89 @@
 #include<stdio.h>
+#include<stdbool.h>
 
-int main(void) {
+#include<string.h>
+
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<unistd.h>
+
+#define MAX_OPTION 4
+char OPTIONS[MAX_OPTION][3]={"-h","-p","-o","-s"};
+
+int sendHello(char* hostName) {
+    int sockFd;
+    struct sockaddr_in serverAddr;
+    struct hostent *server;
+
+    char buffer[256];
+
+    int n;
+
+    sockFd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockFd<0) {
+        printf("creating socket failed\n");
+        return -1;
+    }
+    server = gethostbyname(hostName);
+    if(server == NULL) {
+        printf("getting host name failed\n");
+        return -2;
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family=AF_INET;
+    memcpy((char*)&(serverAddr.sin_addr.s_addr),(char*)server->h_addr,server->h_length);
+    serverAddr.sin_port=3001;
+    if(connect(sockFd,(struct sockaddr*)&serverAddr,sizeof(serverAddr))<0) {
+        printf("connecting failed\n");
+        return -3;
+    }
+    // n=write(sockfd,"Hello",strlen("Hello"));
+    n=send(sockFd,"Hello",strlen("Hello"),0);
+    if(n<0) {
+        printf("writing to socket failed\n");
+        return -4;
+    }
+    close(sockFd);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    unsigned int hostAddress;
+    unsigned int port;
+    bool isEncrypt;
+    unsigned int shift;
+
+    struct sockaddr hostSockaddr;
+
+    int i, j;
+
+    memset(&hostSockaddr,0,sizeof(hostSockaddr));
+
     printf("Hello, this is client\n");
+    printf("argc : %d\n",argc);
+    for(i=0;i<argc;i++) {
+        printf("argv[%d] : %s\n",i,argv[i]);
+    }
+    for(i=1;i<argc;i+=2) {
+        for(j=0;j<4;j++) {
+            if(strncmp(&OPTIONS[j][0],argv[i],3)==0)
+                break;
+        }
+        switch(j) {
+            case 0 : printf("host : %s\n",argv[i+1]);
+                sendHello(argv[i+1]);
+                // hostSockaddr.sa_family=AF_INET;
+                // hostSockaddr.sa_data=
+                 break;
+            case 1 : printf("port : %s\n",argv[i+1]); break;
+            case 2 : printf("isEncrypt : %s\n",argv[i+1]); break;
+            case 3 : printf("shift : %s\n",argv[i+1]); break;
+            default: i=argc;//Unexpected flag
+        }
+    }
+//    ./client -h 143.248.111.222 -p 1234 -o 0 -s 5
+//    HOST. 143.248.111.222, Port: 1234 Operation: encrypt, Shift 5
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,49 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+#include<string.h>
+
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<unistd.h>
 
 int main(void) {
-    printf("This is server.\n");
+    int socketFd, inSocketFd;
+    socklen_t clientLen;
+    char buffer[256];
+    struct sockaddr_in serverAddr, clientAddr;
+    int n;
+
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketFd<0) {
+        printf("creating socket failed\n");
+        return -1;
+    }
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_addr.s_addr=INADDR_ANY;
+    serverAddr.sin_port=3001;
+    if(bind(socketFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr))<0) {
+        printf("binding socket failed\n");
+        return -2;
+    }
+    if(listen(socketFd,5)<0) {
+        printf("listening failed\n");
+        return -3;
+    }
+    clientLen=sizeof(clientAddr);
+    inSocketFd=accept(socketFd,(struct sockaddr *)&clientAddr,&clientLen);
+    if(inSocketFd<0) {
+        printf("accepting failed");
+        return -4;
+    }
+    n = recv(inSocketFd,buffer,256,0);
+    if(n<0) {
+        printf("receiving failed");
+        return -5;
+    }
+    printf("Data Recved : %s\n",buffer);
+    close(inSocketFd);
+    close(socketFd);
     return 0;
 }
